a8793eb0601d95298b195142a987198408fcc0ac
Illia Brytun <elbrytun@gmail.com>
2022-03-24 14:25:53 -0400

    Ill

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <h1>All-The-Things</h1>
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
       <Link to="/the-functional-things">karim's Things</Link><br/>
-      <Link to="/the-well-styled-things">Illia's Things</Link><br/>
+      <Link to="/the-well-styled-things">Ill's Things</Link><br/>
       <Link to="/the-silly-things">SomeoneElse's Things</Link><br/>
     </>
   )
